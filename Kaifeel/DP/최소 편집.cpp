adc6c11bbe6c8af32f896473401881005713dac4
SW <34030977+Kaifeel@users.noreply.github.com>
2026-07-01 18:07:44 +0900

    2026-07-01 최소 편집

--- Kaifeel/DP/최소 편집.cpp
+++ Kaifeel/DP/최소 편집.cpp
@@ -1,29 +1,45 @@
+#include <iostream>
 #include <string>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
-int answer = 0;
-
-void dfs(int total, int start, int end, vector<int> numbers, int target)
+int editDistance(string s1, string s2, int n1, int n2)
 {
-	if (start == end)
-	{
-		if (total == target)
-			answer++;
-		return;
-	}
+	vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
 
-	dfs(total + numbers[start], start + 1, end, numbers, target);
-	dfs(total - numbers[start], start + 1, end, numbers, target);
+	for (int i = 0; i <= n1; i++)
+		dp[i][0] = i;
+	for (int j = 0; j <= n2; j++)
+		dp[0][j] = j;
+
+	for (int i = 1; i <= n1; i++)
+	{
+		for (int j = 1; j <= n2; j++)
+		{
+			if (s1[i - 1] == s2[j - 1])
+			{
+				dp[i][j] = dp[i - 1][j - 1];
+			}
+			else
+			{
+				dp[i][j] = min({dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + 1});
+			}
+		}
+	}
+	return dp[n1][n2];
 }
 
-int solution(vector<int> numbers, int target)
+int main()
 {
+	int n1, n2;
+	string str1, str2;
 
-	int n = numbers.size();
+	cin >> n1 >> str1;
+	cin >> n2 >> str2;
 
-	dfs(0, 0, n, numbers, target);
+	int result = editDistance(str1, str2, n1, n2);
+	cout << result;
 
-	return answer;
+	return 0;
 }
